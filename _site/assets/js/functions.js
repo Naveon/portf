3b800df0c74f5067437418d90eade976498cc06d
Naveon <n.jangid1997@gmail.com>
2017-01-19 23:22:29 +0530

    2

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,24 +1,29 @@
 $(function() {
-    smoothScroll(500);
+    smoothScroll(300);
     workBelt();
     workLoad();
+
+    $("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+    $(".biglink").fitText(1.5);
+
+
 });
 
+// smoothScroll function is applied from the document ready function
 function smoothScroll (duration) {
     $('a[href^="#"]').on('click', function(event) {
 
-        var target = $( $(this).attr('href'));
+        var target = $( $(this).attr('href') );
 
-        if(target.length) {
+        if( target.length ) {
             event.preventDefault();
             $('html, body').animate({
                 scrollTop: target.offset().top
-            },duration);
+            }, duration);
         }
-
     });
-
 }
+
 
 function workBelt() {
 
@@ -50,3 +55,42 @@ function workLoad() {
     });
 
 }
+
+
+
+
+
+(function( $ ){
+
+    $.fn.fitText = function( kompressor, options ) {
+
+        // Setup options
+        var compressor = kompressor || 1,
+            settings = $.extend({
+                'minFontSize' : Number.NEGATIVE_INFINITY,
+                'maxFontSize' : Number.POSITIVE_INFINITY
+            }, options);
+
+        return this.each(function(){
+
+            // Store the object
+            var $this = $(this);
+
+            // Resizer() resizes items based on the object width divided by the compressor * 10
+            var resizer = function () {
+                $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+            };
+
+            // Call once to set.
+            resizer();
+
+            // Call on resize. Opera debounces their resize by default.
+            $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+        });
+
+    };
+
+})( jQuery );
+
+
